491d827b1f4e04bdbfe2dd8fcf43e7c20d391e53
KIMSH <KIMSH@DESKTOP-3GS6KFS>
2017-09-04 11:12:41 +0900

    #1

---
+++ wwft/src/main/java/com/ksh/wwft/WWFTApplication.java
@@ -0,0 +1,12 @@
+package com.ksh.wwft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WWFTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WWFTApplication.class, args);
+	}
+}
---
+++ wwft/src/main/java/com/ksh/wwft/entity/User.java
@@ -0,0 +1,14 @@
+package com.ksh.wwft.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="USER")
+@Getter @Setter
+public class User {
+
+}
